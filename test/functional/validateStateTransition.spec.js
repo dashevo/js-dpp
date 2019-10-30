409065df5a8c91de1b15866e9306e6ae060d204b
Djavid Gabibiyan <djavidoff@gmail.com>
2019-10-30 17:06:06 +0500

    chore(tests): more functional test scenarios

--- test/functional/validateStateTransition.spec.js
+++ test/functional/validateStateTransition.spec.js
@@ -6,12 +6,60 @@ const {
   CommitTransactionRequest,
 } = require('@dashevo/drive-grpc');
 
-const DataContractStateTrnsition = require(
+const {
+  Transaction,
+  PrivateKey,
+} = require('@dashevo/dashcore-lib');
+
+const DataContractStateTransition = require(
   '../../lib/dataContract/stateTransition/DataContractStateTransition',
+);
+const DocumentsStateTransition = require(
+  '../../lib/document/stateTransition/DocumentsStateTransition',
 );
 
 const getDataContractFixture = require('../../lib/test/fixtures/getDataContractFixture');
 const getDocumentsFixture = require('../../lib/test/fixtures/getDocumentsFixture');
+
+async function registerUser(coreApi) {
+  await coreApi.generate(700);
+
+  const { result: addressString } = await coreApi.getNewAddress();
+  const { result: privateKeyString } = await coreApi.dumpPrivKey(addressString);
+
+  const privateKey = new PrivateKey(privateKeyString);
+
+  await coreApi.generate(500);
+  await coreApi.sendToAddress(addressString, 10);
+  await coreApi.generate(10);
+
+  const { result: unspent } = await coreApi.listUnspent();
+  const inputs = unspent.filter(input => input.address === addressString);
+
+  const transactionPayload = new Transaction.Payload.SubTxRegisterPayload();
+
+  const userName = 'dashUser';
+
+  transactionPayload.setUserName(userName)
+    .setPubKeyIdFromPrivateKey(privateKey)
+    .sign(privateKey);
+
+  const transaction = new Transaction({
+    type: Transaction.TYPES.TRANSACTION_SUBTX_REGISTER,
+    version: 3,
+    extraPayload: transactionPayload.toString(),
+  });
+
+  transaction.from(inputs)
+    .addFundingOutput(10000)
+    .change(addressString)
+    .fee(668)
+    .sign(privateKey);
+
+  const { result: userId } = await coreApi.sendrawtransaction(transaction.serialize());
+
+  return userId;
+}
 
 describe('validateStateTransition', function main() {
   this.timeout(180000);
@@ -20,8 +68,10 @@ describe('validateStateTransition', function main() {
 
   let driveApi;
   let driveUpdateStateApi;
+  let coreApi;
 
   let dataContract;
+  let documents;
 
   async function withinTransaction(call) {
     const blockHeight = 0;
@@ -49,12 +99,27 @@ describe('validateStateTransition', function main() {
 
     driveApi = driveInstance.driveApi.getApi();
     driveUpdateStateApi = driveInstance.driveUpdateState.getApi();
+    coreApi = driveInstance.dashCore.getApi();
 
     dataContract = getDataContractFixture();
+    documents = getDocumentsFixture();
+
+    const userId = await registerUser(coreApi);
+
+    dataContract.contractId = userId;
+
+    documents.forEach((d) => {
+      // eslint-disable-next-line
+      d.contractId = userId;
+      // eslint-disable-next-line
+      d.userId = userId;
+    });
   });
 
   it('should not validate contract state transition without a blockchain user', async () => {
-    const stateTransition = new DataContractStateTrnsition(dataContract);
+    dataContract.contractId = Buffer.alloc(32).toString('hex');
+
+    const stateTransition = new DataContractStateTransition(dataContract);
 
     await withinTransaction(async (blockHeight, blockHash) => {
       const request = new ApplyStateTransitionRequest();
@@ -66,10 +131,95 @@ describe('validateStateTransition', function main() {
         await driveUpdateStateApi.applyStateTransition(request);
         expect.fail('Error was not thrown');
       } catch (e) {
-        expect(e.message).to.equal('Invalid State Transition');
+        const [error] = JSON.parse(e.metadata.get('errors')[0]);
+        expect(error.message).to.equal('User not found');
+        expect(e.message).to.equal('3 INVALID_ARGUMENT: Invalid argument: Invalid State Transition');
       }
     });
   });
 
-  afterEach(async () => {});
+  it('should not validate contract state transition when it submitted twice', async () => {
+    const stateTransition = new DataContractStateTransition(dataContract);
+
+    await withinTransaction(async (blockHeight, blockHash) => {
+      const request = new ApplyStateTransitionRequest();
+      request.setStateTransition(stateTransition.serialize());
+      request.setBlockHeight(blockHeight);
+      request.setBlockHash(blockHash);
+
+      await driveUpdateStateApi.applyStateTransition(request);
+    });
+
+    await withinTransaction(async (blockHeight, blockHash) => {
+      const request = new ApplyStateTransitionRequest();
+      request.setStateTransition(stateTransition.serialize());
+      request.setBlockHeight(blockHeight);
+      request.setBlockHash(blockHash);
+
+      try {
+        await driveUpdateStateApi.applyStateTransition(request);
+      } catch (e) {
+        const [error] = JSON.parse(e.metadata.get('errors')[0]);
+        expect(error.message).to.equal('Data Contract is already present');
+      }
+    });
+  });
+
+  it('should successfuly submit valid contract and documents', async () => {
+    const stateTransition = new DataContractStateTransition(dataContract);
+
+    await withinTransaction(async (blockHeight, blockHash) => {
+      const request = new ApplyStateTransitionRequest();
+      request.setStateTransition(stateTransition.serialize());
+      request.setBlockHeight(blockHeight);
+      request.setBlockHash(blockHash);
+
+      await driveUpdateStateApi.applyStateTransition(request);
+    });
+
+    const documentsStateTransition = new DocumentsStateTransition(documents);
+
+    await withinTransaction(async (blockHeight, blockHash) => {
+      const request = new ApplyStateTransitionRequest();
+      request.setStateTransition(documentsStateTransition.serialize());
+      request.setBlockHeight(blockHeight);
+      request.setBlockHash(blockHash);
+
+      await driveUpdateStateApi.applyStateTransition(request);
+    });
+
+    const { result: contract } = await driveApi.request('fetchContract', {
+      contractId: dataContract.getId(),
+    });
+
+    expect(contract).to.deep.equal(dataContract.toJSON());
+
+    const { result: niceDocuments } = await driveApi.request('fetchDocuments', {
+      contractId: dataContract.getId(),
+      type: 'niceDocument',
+    });
+
+    const { result: prettyDocuments } = await driveApi.request('fetchDocuments', {
+      contractId: dataContract.getId(),
+      type: 'prettyDocument',
+    });
+
+    const { result: indexedDocuments } = await driveApi.request('fetchDocuments', {
+      contractId: dataContract.getId(),
+      type: 'indexedDocument',
+    });
+
+    expect(
+      niceDocuments
+        .concat(prettyDocuments)
+        .concat(indexedDocuments),
+    ).to.have.deep.members(
+      documents
+        .map(d => d.toJSON()),
+    );
+  });
+
+  afterEach(async () => {
+    await driveInstance.remove();
+  });
 });
