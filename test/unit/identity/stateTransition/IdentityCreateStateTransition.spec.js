9163eb7dba3d88effa7cfdf809922a26d95c92d6
Djavid Gabibiyan <djavidoff@gmail.com>
2019-11-18 18:33:27 +0500

    update(tests): identity create state transition unit

--- test/unit/identity/stateTransition/IdentityCreateStateTransition.spec.js
+++ test/unit/identity/stateTransition/IdentityCreateStateTransition.spec.js
@@ -1,6 +1,4 @@
-const IdentityCreateStateTransition = require(
-  '../../../../lib/identity/stateTransitions/IdentityCreateStateTransition',
-);
+const rewiremock = require('rewiremock/node');
 
 const stateTransitionTypes = require(
   '../../../../lib/stateTransition/stateTransitionTypes',
@@ -9,11 +7,14 @@ const stateTransitionTypes = require(
 describe('IdentityCreateStateTransition', () => {
   let rawStateTransition;
   let stateTransition;
+  let hashMock;
+  let hashSignerMock;
+  let IdentityCreateStateTransition;
 
-  beforeEach(() => {
+  beforeEach(function beforeEach() {
     rawStateTransition = {
       identityCreateStateTransitionVersion: 1,
-      lockedOutPoint: Buffer.alloc(1),
+      lockedOutPoint: 'c3BlY2lhbEJ1ZmZlcg==',
       identityType: 1,
       publicKeys: [
         {
@@ -23,8 +24,28 @@ describe('IdentityCreateStateTransition', () => {
           isEnabled: true,
         },
       ],
-      ownershipProofSignature: Buffer.alloc(1),
+      ownershipProofSignature: 'c3BlY2lhbEJ1ZmZlcg==',
     };
+
+    hashMock = this.sinonSandbox.stub();
+    hashMock.returns({
+      toString: () => 42,
+    });
+
+    hashSignerMock = {
+      signData: this.sinonSandbox.stub(),
+      verifyDataSignature: this.sinonSandbox.stub(),
+    };
+
+    IdentityCreateStateTransition = rewiremock.proxy(
+      '../../../../lib/identity/stateTransitions/IdentityCreateStateTransition',
+      {
+        '../../../../lib/util/hash': hashMock,
+        '../../../../node_modules/@dashevo/dashcore-lib': {
+          HashSigner: hashSignerMock,
+        },
+      },
+    );
 
     stateTransition = new IdentityCreateStateTransition(rawStateTransition);
   });
@@ -41,11 +62,15 @@ describe('IdentityCreateStateTransition', () => {
     it('should create an instance with specified data from specified raw transition', () => {
       expect(stateTransition.identityCreateStateTransitionVersion).to.equal(1);
       expect(stateTransition.identityType).to.equal(1);
-      expect(stateTransition.lockedOutPoint).to.deep.equal(Buffer.alloc(1));
+      expect(stateTransition.lockedOutPoint).to.deep.equal(
+        rawStateTransition.lockedOutPoint,
+      );
       expect(stateTransition.publicKeys).to.have.deep.members([
         rawStateTransition.publicKeys[0],
       ]);
-      expect(stateTransition.ownershipProofSignature).to.deep.equal(Buffer.alloc(1));
+      expect(stateTransition.ownershipProofSignature).to.deep.equal(
+        rawStateTransition.ownershipProofSignature,
+      );
     });
   });
 
@@ -73,11 +98,26 @@ describe('IdentityCreateStateTransition', () => {
       stateTransition.setLockedOutPoint(Buffer.alloc(42, 3));
       expect(stateTransition.lockedOutPoint).to.deep.equal(Buffer.alloc(42, 3));
     });
+
+    it('should set `identityId`', () => {
+      hashMock.reset();
+      hashMock.returns({
+        toString: () => 42,
+      });
+
+      stateTransition = new IdentityCreateStateTransition();
+      stateTransition.setLockedOutPoint(Buffer.alloc(0).toString('base64'));
+
+      expect(hashMock).to.have.been.calledOnceWith(Buffer.alloc(0));
+      expect(stateTransition.identityId).to.equal(42);
+    });
   });
 
   describe('#getLockedOutPoint', () => {
     it('should return currently set locked OutPoint', () => {
-      expect(stateTransition.getLockedOutPoint()).to.deep.equal(Buffer.alloc(1));
+      expect(stateTransition.getLockedOutPoint()).to.deep.equal(
+        rawStateTransition.lockedOutPoint,
+      );
     });
   });
 
@@ -117,6 +157,33 @@ describe('IdentityCreateStateTransition', () => {
     });
   });
 
+  describe('#signOwnershipProof', () => {
+    it('should set ownership prood signature', () => {
+      hashSignerMock.signData.returns(42);
+      stateTransition.signOwnershipProof('wowMuchPrivateKey');
+
+      expect(hashSignerMock.signData).to.have.been.calledOnceWith(
+        Buffer.from('specialBuffer'),
+        'wowMuchPrivateKey',
+      );
+      expect(stateTransition.ownershipProofSignature).to.equal(42);
+    });
+  });
+
+  describe('#verifyOwnershipProofSignature', () => {
+    it('should call HashSigner and return boolean', () => {
+      hashSignerMock.verifyDataSignature.returns(true);
+      const result = stateTransition.verifyOwnershipProofSignature('wowMuchPublicKeyHash');
+
+      expect(hashSignerMock.verifyDataSignature).to.have.been.calledOnceWith(
+        Buffer.from('specialBuffer'),
+        Buffer.from('specialBuffer'),
+        'wowMuchPublicKeyHash',
+      );
+      expect(result).to.be.true();
+    });
+  });
+
   describe('#setOwnershipProofSignature', () => {
     it('should set ownership proof signature', () => {
       stateTransition.setOwnershipProofSignature(Buffer.alloc(42, 3));
@@ -127,7 +194,15 @@ describe('IdentityCreateStateTransition', () => {
   describe('#getOwnershipProofSignature', () => {
     it('should return set ownership proof signature', () => {
       expect(stateTransition.getOwnershipProofSignature()).to.deep.equal(
-        Buffer.alloc(1),
+        rawStateTransition.ownershipProofSignature,
+      );
+    });
+  });
+
+  describe('#getIdentityId', () => {
+    it('should return set identity id', () => {
+      expect(stateTransition.getIdentityId()).to.equal(
+        42,
       );
     });
   });
