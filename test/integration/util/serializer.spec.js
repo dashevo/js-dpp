ba7040d6cb5ad75746a4f5add3f829ecfb537520
Ivan Shumkov <ivan@shumkov.ru>
2020-02-19 21:47:54 +0300

    chore: rename error

--- test/integration/util/serializer.spec.js
+++ test/integration/util/serializer.spec.js
@@ -1,6 +1,6 @@
 const { encode } = require('../../../lib/util/serializer');
 
-const DataSerializationError = require('../../../lib/util/errors/DataSerializationError');
+const DataSerializationError = require('../../../lib/util/errors/MaxEncodedBytesReachedError');
 
 describe('serializer', function main() {
   this.timeout(10000);
