a8da3ed88a7463af4a01dd9c734669a934008a04
shuplenkov <konstantin.shuplenkov@dash.org>
2020-03-24 15:38:37 +0300

    Unit tests

--- test/unit/identity/creditsConverter.spec.js
+++ test/unit/identity/creditsConverter.spec.js
@@ -1,8 +1,26 @@
+const {
+  convertSatoshiToCredits,
+  convertCreditsToSatoshi,
+  CONVERT_RATIO,
+} = require('../../../lib/identity/creditsConverter');
+
 describe('creditsConverter', () => {
   describe('convertSatoshiToCredits', () => {
-    it('should convert satoshi to credits');
+    it('should convert satoshi to credits', () => {
+      const amount = 42;
+
+      const convertedAmount = convertSatoshiToCredits(amount);
+
+      expect(convertedAmount).to.equal(amount * CONVERT_RATIO);
+    });
   });
   describe('convertCreditsToSatoshi', () => {
-    it('should convert credits to satoshi');
+    it('should convert credits to satoshi', () => {
+      const amount = 4242;
+
+      const convertedAmount = convertCreditsToSatoshi(amount);
+
+      expect(convertedAmount).to.equal(Math.ceil(amount / CONVERT_RATIO));
+    });
   });
 });
