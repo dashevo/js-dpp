8cb0977d13f8eab0c7ee49a408fa1b410a193ac9
shuplenkov <konstantin.shuplenkov@dash.org>
2020-04-15 17:11:01 +0300

    Some fixes

--- test/unit/util/version.spec.js
+++ test/unit/util/version.spec.js
@@ -9,16 +9,21 @@ describe('version', () => {
       ['1.0.12', '1.0.12'],
       ['0.12.0-dev.8', '0.12.0'],
       ['10.15.3-build-123', '10.15.3'],
+      ['1.345.6.dev', '1.345.6'],
+      ['10.15.3.build-123', '10.15.3'],
+      ['0.0.0.1', '0.0.0'],
     ];
   });
 
-  it('should return canonical version', async () => {
-    versionsToCheck.forEach(([version, protocolVersion]) => {
-      const { getProtocolVersion } = rewiremock.proxy('../../../lib/util/version', {
-        '../../../package.json': { version },
-      });
+  describe('#getProtocolVersion', () => {
+    it('should return canonical version', async () => {
+      versionsToCheck.forEach(([version, protocolVersion]) => {
+        const { getProtocolVersion } = rewiremock.proxy('../../../lib/util/version', {
+          '../../../package.json': { version },
+        });
 
-      expect(getProtocolVersion()).to.equal(protocolVersion);
+        expect(getProtocolVersion()).to.equal(protocolVersion);
+      });
     });
   });
 });
