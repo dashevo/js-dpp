1181ab744ba7d0c65f1fed2e1c60efc7a92ee258
Anton Suprunchuk <antouhou@gmail.com>
2020-04-26 16:12:33 +0300

    Brought back identity st create validation and tests

--- test/integration/identity/stateTransition/identityCreateTransition/validateIdentityCreateSTStructureFactory.spec.js
+++ test/integration/identity/stateTransition/identityCreateTransition/validateIdentityCreateSTStructureFactory.spec.js
@@ -1,12 +1,20 @@
+const Ajv = require('ajv');
+
 const getIdentityCreateSTFixture = require('../../../../../lib/test/fixtures/getIdentityCreateSTFixture');
 
 const validateIdentityCreateSTStructureFactory = require(
   '../../../../../lib/identity/stateTransitions/identityCreateTransition/validateIdentityCreateSTStructureFactory',
 );
 
+const JsonSchemaValidator = require(
+  '../../../../../lib/validation/JsonSchemaValidator',
+);
+
 const IdentityCreateTransition = require(
   '../../../../../lib/identity/stateTransitions/identityCreateTransition/IdentityCreateTransition',
 );
+
+const { expectJsonSchemaError } = require('../../../../../lib/test/expect/expectError');
 
 const ValidationResult = require('../../../../../lib/validation/ValidationResult');
 
@@ -17,9 +25,13 @@ describe('validateIdentityCreateSTStructureFactory', () => {
   let validatePublicKeysMock;
 
   beforeEach(function beforeEach() {
+    const ajv = new Ajv();
+    const validator = new JsonSchemaValidator(ajv);
+
     validatePublicKeysMock = this.sinonSandbox.stub().returns(new ValidationResult());
 
     validateIdentityCreateST = validateIdentityCreateSTStructureFactory(
+      validator,
       validatePublicKeysMock,
     );
 
@@ -46,5 +58,57 @@ describe('validateIdentityCreateSTStructureFactory', () => {
     expect(validatePublicKeysMock).to.be.calledOnceWithExactly(
       rawStateTransition.publicKeys,
     );
+  });
+
+  describe('publicKeys', () => {
+    it('should be present', () => {
+      rawStateTransition.publicKeys = undefined;
+
+      const result = validateIdentityCreateST(rawStateTransition);
+
+      expectJsonSchemaError(result);
+
+      const [error] = result.getErrors();
+
+      expect(error.dataPath).to.equal('');
+      expect(error.params.missingProperty).to.equal('publicKeys');
+      expect(error.keyword).to.equal('required');
+
+      expect(validatePublicKeysMock).to.not.be.called();
+    });
+
+    it('should not be empty', () => {
+      rawStateTransition.publicKeys = [];
+
+      const result = validateIdentityCreateST(rawStateTransition);
+
+      expectJsonSchemaError(result);
+
+      const [error] = result.getErrors();
+
+      expect(error.keyword).to.equal('minItems');
+      expect(error.dataPath).to.equal('.publicKeys');
+
+      expect(validatePublicKeysMock).to.not.be.called();
+    });
+
+    it('should not have more than 10 items', () => {
+      const [key] = rawStateTransition.publicKeys;
+
+      for (let i = 0; i < 10; i++) {
+        rawStateTransition.publicKeys.push(key);
+      }
+
+      const result = validateIdentityCreateST(rawStateTransition);
+
+      expectJsonSchemaError(result);
+
+      const [error] = result.getErrors();
+
+      expect(error.keyword).to.equal('maxItems');
+      expect(error.dataPath).to.equal('.publicKeys');
+
+      expect(validatePublicKeysMock).to.not.be.called();
+    });
   });
 });
