a2df4e0505528e34e9f6eb79f94202511cbc5c73
shuplenkov <konstantin.shuplenkov@dash.org>
2020-05-14 17:33:27 +0300

    Check that locked tx is finalized

--- test/unit/DashPlatformProtocol.spec.js
+++ test/unit/DashPlatformProtocol.spec.js
@@ -9,14 +9,17 @@ describe('DashPlatformProtocol', () => {
   let dpp;
   let stateRepositoryMock;
   let jsonSchemaValidatorMock;
+  let useLockTxFallback;
 
   beforeEach(function beforeEach() {
     stateRepositoryMock = createStateRepositoryMock(this.sinonSandbox);
     jsonSchemaValidatorMock = {};
+    useLockTxFallback = true;
 
     dpp = new DashPlatformProtocol({
       stateRepository: stateRepositoryMock,
       jsonSchemaValidator: jsonSchemaValidatorMock,
+      useLockTxFallback,
     });
   });
 
@@ -44,6 +47,12 @@ describe('DashPlatformProtocol', () => {
       const result = dpp.getJsonSchemaValidator();
 
       expect(result).to.equal(jsonSchemaValidatorMock);
+    });
+  });
+
+  describe('useLockTxFallback', () => {
+    it('should contain useLockTxFallback value', async () => {
+      expect(dpp.useLockTxFallback).to.equal(useLockTxFallback);
     });
   });
 });
