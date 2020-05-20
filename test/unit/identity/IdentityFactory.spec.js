c71267ae7a3cd168596048e7525cea10f12595d6
Anton Suprunchuk <antouhou@gmail.com>
2020-05-20 23:15:38 +0300

    Add random stuff

--- test/unit/identity/IdentityFactory.spec.js
+++ test/unit/identity/IdentityFactory.spec.js
@@ -173,6 +173,7 @@ describe('IdentityFactory', () => {
     it('should create IdentityTopUpTransition from identity id and outpoint', () => {
       const lockedOutPoint = crypto.randomBytes(64);
 
+
       identity.setLockedOutPoint(lockedOutPoint);
 
       const stateTransition = factory
