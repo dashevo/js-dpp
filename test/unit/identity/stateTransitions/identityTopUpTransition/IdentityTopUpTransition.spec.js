01b9b37ec8bde737d3cd89617fe80cd611cc4b02
Anton Suprunchuk <antouhou@gmail.com>
2020-05-20 19:37:43 +0300

    Add correct raw state transition to class test

--- test/unit/identity/stateTransitions/identityTopUpTransition/IdentityTopUpTransition.spec.js
+++ test/unit/identity/stateTransitions/identityTopUpTransition/IdentityTopUpTransition.spec.js
@@ -16,14 +16,7 @@ describe('IdentityTopUpTransition', () => {
   beforeEach(function beforeEach() {
     rawStateTransition = {
       lockedOutPoint: 'c3BlY2lhbEJ1ZmZlcg==',
-      publicKeys: [
-        {
-          id: 0,
-          type: IdentityPublicKey.TYPES.ECDSA_SECP256K1,
-          data: 'someString',
-          isEnabled: true,
-        },
-      ],
+      identityId: '9egkkRs6ErFbLUh3yYn8mdgeKGpJQ41iayS1Z9bwsRM7',
     };
 
     hashMock = this.sinonSandbox.stub();
@@ -75,17 +68,6 @@ describe('IdentityTopUpTransition', () => {
       stateTransition.setLockedOutPoint(Buffer.alloc(42, 3));
       expect(stateTransition.lockedOutPoint).to.deep.equal(Buffer.alloc(42, 3));
     });
-
-    it('should set `identityId`', () => {
-      hashMock.reset();
-      hashMock.returns(Buffer.alloc(32));
-
-      stateTransition = new IdentityTopUpTransition();
-      stateTransition.setLockedOutPoint(Buffer.alloc(0).toString('base64'));
-
-      expect(hashMock).to.have.been.calledOnceWith(Buffer.alloc(0));
-      expect(stateTransition.identityId).to.equal('11111111111111111111111111111111');
-    });
   });
 
   describe('#getLockedOutPoint', () => {
@@ -96,38 +78,10 @@ describe('IdentityTopUpTransition', () => {
     });
   });
 
-  describe('#setPublicKeys', () => {
-    it('should set public keys', () => {
-      const publicKeys = [new IdentityPublicKey(), new IdentityPublicKey()];
-
-      stateTransition.setPublicKeys(publicKeys);
-
-      expect(stateTransition.publicKeys).to.have.deep.members(publicKeys);
-    });
-  });
-
-  describe('#getPublicKeys', () => {
-    it('should return set public keys', () => {
-      expect(stateTransition.getPublicKeys()).to.deep.equal(
-        rawStateTransition.publicKeys.map((rawPublicKey) => new IdentityPublicKey(rawPublicKey)),
-      );
-    });
-  });
-
-  describe('#addPublicKeys', () => {
-    it('should add more public keys', () => {
-      const publicKeys = [new IdentityPublicKey(), new IdentityPublicKey()];
-
-      stateTransition.publicKeys = [];
-      stateTransition.addPublicKeys(publicKeys);
-      expect(stateTransition.getPublicKeys()).to.have.deep.members(publicKeys);
-    });
-  });
-
   describe('#getIdentityId', () => {
     it('should return identity id', () => {
       expect(stateTransition.getIdentityId()).to.equal(
-        '11111111111111111111111111111111',
+        '9egkkRs6ErFbLUh3yYn8mdgeKGpJQ41iayS1Z9bwsRM7',
       );
     });
   });
