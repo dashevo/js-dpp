e963462cf4c1cb534a6a84b30912b1454712af0e
Djavid Gabibiyan <djavidoff@gmail.com>
2020-05-22 11:16:07 +0500

    test: new validation function

---
+++ test/unit/identity/stateTransitions/identityCreateTransition/validateIdentityPublicKeyUniquenessFactory.spec.js
@@ -0,0 +1,52 @@
+const validateIdentityPublicKeyUniquenessFactory = require(
+  '../../../../../lib/identity/stateTransitions/identityCreateTransition/validateIdentityPublicKeyUniquenessFactory',
+);
+
+const IdentityPublicKeyAlreadyExistsError = require(
+  '../../../../../lib/errors/IdentityPublicKeyAlreadyExistsError',
+);
+
+const getIdentityFixture = require('../../../../../lib/test/fixtures/getIdentityFixture');
+const createStateRepositoryMock = require('../../../../../lib/test/mocks/createStateRepositoryMock');
+
+const { expectValidationError } = require(
+  '../../../../../lib/test/expect/expectError',
+);
+
+describe('validateLockTransactionFactory', () => {
+  let identity;
+  let stateRepositoryMock;
+  let validateIdentityPublicKeyUniqueness;
+
+  beforeEach(function beforeEach() {
+    identity = getIdentityFixture();
+
+    stateRepositoryMock = createStateRepositoryMock(this.sinonSandbox);
+
+    validateIdentityPublicKeyUniqueness = validateIdentityPublicKeyUniquenessFactory(
+      stateRepositoryMock,
+    );
+  });
+
+  it('should return invalid result if identity id was found', async () => {
+    stateRepositoryMock.fetchPublicKeyIdentityId.resolves(identity.getId());
+
+    const result = await validateIdentityPublicKeyUniqueness(
+      identity.getPublicKeyById(0),
+    );
+
+    expectValidationError(result, IdentityPublicKeyAlreadyExistsError);
+
+    const [error] = result.getErrors();
+
+    expect(error.getPublicKeyHash()).to.equal(identity.getPublicKeyById(0).hash());
+  });
+
+  it('should return valid result if no identity id was not found', async () => {
+    const result = await validateIdentityPublicKeyUniqueness(
+      identity.getPublicKeyById(0),
+    );
+
+    expect(result.isValid()).to.be.true();
+  });
+});
