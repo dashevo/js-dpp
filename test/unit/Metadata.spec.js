7e4076084de7d2b68421849b02c871bab0c13560
Djavid Gabibiyan <djavidoff@gmail.com>
2021-06-29 22:56:23 +0500

    chore: pr fixes

--- test/unit/Metadata.spec.js
+++ test/unit/Metadata.spec.js
@@ -3,42 +3,33 @@ const Metadata = require('../../lib/Metadata');
 describe('Metadata', () => {
   describe('#constructor', () => {
     it('should set height and core chain-locked height', () => {
-      const result = new Metadata(42, 1);
+      const result = new Metadata({
+        blockHeight: 42,
+        coreChainLockedHeight: 1,
+      });
 
       expect(result.blockHeight).to.equal(42);
       expect(result.coreChainLockedHeight).to.equal(1);
     });
   });
 
-  describe('#setBlockHeight', () => {
-    it('should set block height', () => {
-      const result = new Metadata();
-      result.setBlockHeight(42);
-
-      expect(result.blockHeight).to.equal(42);
-    });
-  });
-
   describe('#getBlockHeight', () => {
     it('should get block height', () => {
-      const result = new Metadata(42, 0);
+      const result = new Metadata({
+        blockHeight: 42,
+        coreChainLockedHeight: 1,
+      });
 
       expect(result.getBlockHeight()).to.equal(42);
     });
   });
 
-  describe('#setCoreChainLockedHeight', () => {
-    it('should set core chain-locked height', () => {
-      const result = new Metadata();
-      result.setCoreChainLockedHeight(42);
-
-      expect(result.coreChainLockedHeight).to.equal(42);
-    });
-  });
-
   describe('#getCoreChainLockedHeight', () => {
     it('should get core chain-locked height', () => {
-      const result = new Metadata(0, 42);
+      const result = new Metadata({
+        blockHeight: 1,
+        coreChainLockedHeight: 42,
+      });
 
       expect(result.getCoreChainLockedHeight()).to.equal(42);
     });
